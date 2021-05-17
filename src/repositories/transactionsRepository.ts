ec4f17f9093cfc5e7e18859f12fb6b31f6de0b8a
Edson Moita <moitaneto@hotmail.com>
2021-05-17 20:50:03 -0300

    feat(transactions): :sparkles: add transactions repository

---
+++ src/repositories/transactionsRepository.ts
@@ -0,0 +1,47 @@
+import { startOfDay, endOfDay } from 'date-fns';
+
+import Transaction from '../models/Transaction';
+import ITransaction from '../types/ITransaction';
+
+type TransactionCreationParams = {
+  account_id: number;
+  value: number;
+  date: Date;
+};
+
+export async function create(
+  transactionData: TransactionCreationParams,
+): Promise<ITransaction> {
+  return Transaction.create(transactionData);
+}
+
+export async function listAllFromAccount(
+  account_id: number,
+): Promise<ITransaction[]> {
+  return Transaction.find({ account_id });
+}
+
+type TotalWithdrawalsFromAccountInDayParams = {
+  account_id: number;
+  date: Date;
+};
+
+export async function getTotalWithdrawalsFromAccountInDay({
+  account_id,
+  date,
+}: TotalWithdrawalsFromAccountInDayParams) {
+  const result = await Transaction.aggregate()
+    .match({
+      account_id,
+      value: { $lt: 0 },
+      date: { $gte: startOfDay(date), $lte: endOfDay(date) },
+    })
+    .group({
+      _id: { _id: '$account_id' },
+      total: { $sum: '$value' },
+    });
+
+  if (!(result && result.length)) return 0;
+
+  return -result[0].total;
+}
