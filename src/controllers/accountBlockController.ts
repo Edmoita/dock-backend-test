565872a384a7b0bdd2fe574f6cca7adb93a03a07
Edson Moita <moitaneto@hotmail.com>
2021-05-17 20:45:15 -0300

    feat(accounts): :sparkles: add account block controller

---
+++ src/controllers/accountBlockController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+import blockAccount from '../services/blockAccountService';
+
+export async function create(
+  request: Request,
+  response: Response,
+): Promise<Response> {
+  const { account_id } = request.params;
+
+  const account = await blockAccount(Number(account_id));
+
+  return response.json(account);
+}
