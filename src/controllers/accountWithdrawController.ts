4060264dda7dc4739a7ff3517b5e9f8f0642db55
Edson Moita <moitaneto@hotmail.com>
2021-05-17 20:51:57 -0300

    feat(transactions): :sparkles: add account withdraw controller

---
+++ src/controllers/accountWithdrawController.ts
@@ -0,0 +1,18 @@
+import { Request, Response } from 'express';
+
+import createAccountWithdraw from '../services/createAccountWithdrawService';
+
+export async function create(
+  request: Request,
+  response: Response,
+): Promise<Response> {
+  const { account_id, value, date = new Date() } = request.body;
+
+  const transaction = await createAccountWithdraw({
+    account_id,
+    value,
+    date: new Date(date),
+  });
+
+  return response.json(transaction);
+}
