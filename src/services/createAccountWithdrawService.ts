66019a064f91f75d7256fdea0a75e6ef86881b32
Edson Moita <moitaneto@hotmail.com>
2021-05-17 20:55:13 -0300

    feat(transactions): :sparkles: add service for creating account withdraw

---
+++ src/services/createAccountWithdrawService.ts
@@ -0,0 +1,48 @@
+import {
+  ACCOUNT_BLOCKED,
+  DAILY_WITHDRAWAL_LIMIT_EXCEEDED,
+  INSUFFICIENT_FUNDS,
+} from '../middlewares/errorHandling/errors/AccountErrors';
+import * as accountsRepository from '../repositories/accountsRepository';
+import * as transactionsRepository from '../repositories/transactionsRepository';
+import ITransaction from '../types/ITransaction';
+
+type Params = {
+  account_id: number;
+  value: number;
+  date: Date;
+};
+
+export default async ({
+  account_id,
+  value,
+  date,
+}: Params): Promise<ITransaction> => {
+  const account = await accountsRepository.findByIdOrFail(account_id);
+
+  if (!account.is_active) throw ACCOUNT_BLOCKED;
+
+  if (account.balance < value) throw INSUFFICIENT_FUNDS;
+
+  const totalWithdrawal =
+    await transactionsRepository.getTotalWithdrawalsFromAccountInDay({
+      account_id,
+      date,
+    });
+
+  if (totalWithdrawal + value > account.daily_withdrawal_limit) {
+    throw DAILY_WITHDRAWAL_LIMIT_EXCEEDED;
+  }
+
+  const transaction = await transactionsRepository.create({
+    account_id,
+    value: -value,
+    date,
+  });
+
+  account.balance -= value;
+
+  await accountsRepository.save(account);
+
+  return transaction;
+};
