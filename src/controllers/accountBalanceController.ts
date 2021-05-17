25bfd9b91c19dd8050427ef8a45f82c7c45777e4
Edson Moita <moitaneto@hotmail.com>
2021-05-17 20:44:42 -0300

    feat(accounts): :sparkles: add account balance controller

---
+++ src/controllers/accountBalanceController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+import readAccountBalance from '../services/readAccountBalanceService';
+
+export async function read(
+  request: Request,
+  response: Response,
+): Promise<Response> {
+  const { account_id } = request.params;
+
+  const balance = await readAccountBalance(Number(account_id));
+
+  return response.json(balance);
+}
