08deeb1d7e17b97db9ee60eaa4d914049e2c14df
Edson Moita <moitaneto@hotmail.com>
2021-05-17 20:45:54 -0300

    feat(transactions): :sparkles: add account deposit controller

---
+++ src/controllers/accountDepositController.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+
+import createAccountDeposit from '../services/createAccountDepositService';
+
+export async function create(
+  request: Request,
+  response: Response,
+): Promise<Response> {
+  const { account_id, value, date } = request.body;
+
+  const transaction = await createAccountDeposit({ account_id, value, date });
+
+  return response.json(transaction);
+}
