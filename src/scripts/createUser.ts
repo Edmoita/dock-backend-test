d7b5a4adff67a4f0bf034944aea5ff953d7a8241
Edson Moita <moitaneto@hotmail.com>
2021-05-18 17:14:59 -0300

    feat(users): :sparkles: adapt script to mongo in docer

--- src/scripts/createUser.ts
+++ src/scripts/createUser.ts
@@ -4,18 +4,23 @@ import dbConfig from '../db/config';
 import User from '../models/User';
 
 async function createUser(): Promise<void> {
-  const uri = 'mongodb://localhost:27017/dock_mongo';
+  try {
+    const uri = 'mongodb://db:27017/dock_mongo';
 
-  await mongoose.connect(uri, dbConfig.MONGOOSE_OPTS);
+    await mongoose.connect(uri, dbConfig.MONGOOSE_OPTS);
 
-  await User.create({
-    _id: 1,
-    name: 'Usuário de teste',
-    cpf: '00011122233',
-    date_of_birth: new Date(1995, 11, 17),
-  });
+    const user = await User.create({
+      name: 'Usuário de teste',
+      cpf: '00011122233',
+      date_of_birth: new Date(1995, 11, 17),
+    });
 
-  process.exit();
+    console.log('Usuário criado', user);
+  } catch (error) {
+    console.error('Erro ao executar script:', error.message);
+  } finally {
+    process.exit();
+  }
 }
 
 createUser();
