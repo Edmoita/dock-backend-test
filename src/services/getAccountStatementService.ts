b8c64c4518e284c4a536d35a8a5e11d2078d8574
Edson Moita <moitaneto@hotmail.com>
2021-05-18 09:56:02 -0300

    feat(transactions): :sparkles: add check for inactive account

--- src/services/getAccountStatementService.ts
+++ src/services/getAccountStatementService.ts
@@ -1,3 +1,4 @@
+import { ACCOUNT_BLOCKED } from '../middlewares/errorHandling/errors/AccountErrors';
 import * as accountsRepository from '../repositories/accountsRepository';
 import * as transactionsRepository from '../repositories/transactionsRepository';
 import ITransaction from '../types/ITransaction';
@@ -9,9 +10,12 @@ type Params = {
 export default async ({ account_id }: Params): Promise<ITransaction[]> => {
   const account = await accountsRepository.findByIdOrFail(account_id);
 
-  const transactions = await transactionsRepository.listAllFromAccount(
-    account._id,
-  );
+  if (!account.is_active) throw ACCOUNT_BLOCKED;
+
+  const transactions =
+    await transactionsRepository.listAllFromAccountSortedByDateDesc(
+      account._id,
+    );
 
   return transactions;
 };
