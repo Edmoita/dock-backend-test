08cef0eaa6d266273d7d117b9bddb1ea6b2a00c3
Edson Moita <moitaneto@hotmail.com>
2021-05-18 17:03:30 -0300

    feat: :memo: add endpoint /docs for swagger documentation

--- src/app.ts
+++ src/app.ts
@@ -4,9 +4,11 @@ import express from 'express';
 import cors from 'cors';
 import compression from 'compression';
 import 'express-async-errors';
+import swaggerUi from 'swagger-ui-express';
 
 import routes from './routes';
 import handleError from './middlewares/errorHandling';
+import swaggerDocument from './swagger.json';
 
 const app = express();
 
@@ -19,6 +21,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get('/', (_, res) => {
   return res.json({ message: 'Dock Test API' });
