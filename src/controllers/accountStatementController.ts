9c3ad6384db2c399726a129062d2c9e2c1b2b511
Edson Moita <moitaneto@hotmail.com>
2021-05-18 12:47:00 -0300

    feat(transactions): :sparkles: add variables to handle statement by period

--- src/controllers/accountStatementController.ts
+++ src/controllers/accountStatementController.ts
@@ -6,10 +6,12 @@ export async function read(
   request: Request,
   response: Response,
 ): Promise<Response> {
-  const { account_id } = request.query;
+  const { account_id, beginDate, endDate } = request.query;
 
   const transaction = await getAccountStatement({
     account_id: Number(account_id),
+    beginDate: beginDate as string,
+    endDate: endDate as string,
   });
 
   return response.json(transaction);
